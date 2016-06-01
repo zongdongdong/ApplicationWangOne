5cbabf3b0c4e1567fcbe7805c6e78cb21075f73f
zongdongdong <785437085@qq.com>
2016-06-01 20:15:03 +0800

    更新

--- app/src/main/java/com/uni/applicationwangone/ui/NewFragment/ProtectionDeviceStytemFragment.java
+++ app/src/main/java/com/uni/applicationwangone/ui/NewFragment/ProtectionDeviceStytemFragment.java
@@ -62,7 +62,7 @@ public class ProtectionDeviceStytemFragment extends BaseFragment {
     @OnClick({R.id.btn1, R.id.btn2,R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn1:
+            case R.id.btn7:
                 goMainActivity();
                 break;
             default:
